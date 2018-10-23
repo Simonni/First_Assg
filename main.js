9332ba2c86f668d270874334a8b7035fe80f5768
Simonni <szkiflay@gmal.com>
2018-10-23 10:41:54 -0400

    adding assg

---
+++ main.js
@@ -0,0 +1,17 @@
+
+let welcome = prompt("Who are you?")
+console.log(welcome)  
+
+let  name = confirm("Write your first and last Name")
+if (name == true) {
+  txt = "You pressed OK!";
+} else {
+  txt = "You pressed Cancel!";
+}
+console.log(txt)
+
+let press = prompt("Write here!")
+console.log(press)
+
+let a=alert("You are Awesome!");
+console.log("You are Awesome!")
